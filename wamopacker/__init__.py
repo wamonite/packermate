51db63131f5bc7ce97397f7ceecb9c72a00937b5
Warren Moore <warren@wamonite.com>
2015-12-03 21:52:10 +0000

    Update setup.py.

--- wamopacker/__init__.py
+++ wamopacker/__init__.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__title__ = 'wamopacker'
+__version__ = '0.0.0'
+__author__ = 'Warren Moore'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2015 Warren Moore'
+
 from .command import Builder, BuilderException
 from .config import Config, ConfigException
