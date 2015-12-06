ac3604bee56e8570486202900b0fa421a709e7ec
Warren Moore <warren@wamonite.com>
2015-12-06 19:53:58 +0000

    Bump version: 0.1.0 → 0.2.0

--- wamopacker/__init__.py
+++ wamopacker/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'wamopacker'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Warren Moore'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2015 Warren Moore'
